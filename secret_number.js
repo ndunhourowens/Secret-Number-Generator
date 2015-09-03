1be58a4addb47f31e046109a3d32726a178022d3
Naomi Owens <naomiowens@Naomis-Mac-mini.local>
2015-09-02 17:19:47 -1000

    finished code challange

--- secret_number.js
+++ secret_number.js
@@ -1,4 +1,7 @@
 'use strict';
 module.exports = function() {
-  
+  var secretNumberGenerator = Math.floor(Math.random() * 1000000);
+  return function () {
+    return secretNumberGenerator;
+  };
 };
